9c4e2e44ea2df49f40131e68ef5b77e1f948fee8
Ryan Natvig <54182194+ryanrocksforever@users.noreply.github.com>
2024-03-05 12:20:37 -0800

    sizing

--- js/script.js
+++ js/script.js
@@ -19,11 +19,19 @@ domReady(function () {
     location.replace("confirmationPage.html")
   }
 
-  let htmlscanner = new Html5QrcodeScanner(
-    "my-qr-reader",
-    { fps: 10, qrbos: 250, qrbox: {width:250, height:250} }
-  );
-  htmlscanner.render(onScanSuccess);
-  console.log("stopDisp-lay");
+  // let htmlscanner = new Html5QrcodeScanner(
+  //   "my-qr-reader",
+  //   { fps: 10, qrbos: 250, qrbox: {width:250, height:250} }
+  // );
+  // htmlscanner.render(onScanSuccess);
+  // console.log("stopDisp-lay");
+
+  const html5QrCode = new Html5Qrcode("my-qr-reader");
+  const qrCodeSuccessCallback = (decodedText, decodedResult) => {
+    /* handle success */
+  };
+  const config = { fps: 10, qrbox: { width: 250, height: 250 } };
+  html5QrCode.start({ facingMode: "environment" }, config, onScanSuccess);
+  html5QrCode.render(onScanSuccess)
   //document.querySelector("#html5-qrcode-button-camera-stop").display = "none";
 });
